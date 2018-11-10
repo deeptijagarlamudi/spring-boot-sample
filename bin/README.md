5c9d641c812250d4c6a2b8fa14ac7a9bdda6ed2f
Deepthi <deepti.jagarlamudi@gmail.com>
2018-11-10 22:18:18 +0530

    removing unnecessary files

--- bin/README.md
+++
@@ -1,2 +0,0 @@
-# spring-boot-sample
-This is simple project with spring boot project structure and rest end points
--- target/classes/data.sql
+++
@@ -1,7 +0,0 @@
-insert into employee
- values(1, 'employee1', 'employeeaddress1', 1000, CURRENT_TIMESTAMP, 'user1', CURRENT_TIMESTAMP, 'user2');
-
- 
- insert into employee
- values(2, 'employee2', 'employeeaddress2', 1000, CURRENT_TIMESTAMP, 'user2', CURRENT_TIMESTAMP, 'user2');
- 
